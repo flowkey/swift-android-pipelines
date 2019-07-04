0f5dd9b4a37369968a0c04213ff88aa942004dd7
Geordie J <geojay@gmail.com>
2019-07-04 19:45:28 +0200

    Clean up

--- cmake/caches/android-armv7.cmake
+++ cmake/caches/android-armv7.cmake
@@ -1,25 +1,9 @@
-
 set(CMAKE_SYSTEM_NAME Android CACHE STRING "")
-# set(CMAKE_SYSTEM_VERSION 21 CACHE STRING "")
+set(CMAKE_SYSTEM_VERSION 21 CACHE STRING "")
 
-set(CMAKE_ANDROID_API 21 CACHE STRING "")
 set(CMAKE_ANDROID_ARCH_ABI armeabi-v7a CACHE STRING "")
 set(CMAKE_ANDROID_NDK_TOOLCHAIN_VERSION clang CACHE STRING "")
-
-# if(CMAKE_HOST_SYSTEM_NAME STREQUAL Windows)
-#   if(EXISTS C:/Microsoft/AndroidNDK64/android-ndk-r16b)
-#     set(CMAKE_ANDROID_NDK "C:/Microsoft/AndroidNDK64/android-ndk-r16b" CACHE FILEPATH "")
-#   elseif(EXISTS C:/Microsoft/AndroidNDK64/android-ndk-r15c)
-#     set(CMAKE_ANDROID_NDK "C:/Microsoft/AndroidNDK64/android-ndk-r15c" CACHE FILEPATH "")
-#   else()
-#     message(FATAL_ERROR "unable to find android NDK")
-#   endif()
-# else()
-# # CMAKE_ANDROID_NDK is set from env var / pipeline variable ANDROID_NDK
-# endif()
-
-# set(CMAKE_ANDROID_STL_TYPE c++_static CACHE STRING "")
+set(CMAKE_ANDROID_STL_TYPE c++_shared CACHE STRING "")
 
 set(CMAKE_ANDROID_ARM_MODE NO CACHE BOOL "")
 set(CMAKE_ANDROID_ARM_NEON YES CACHE BOOL "")
-
