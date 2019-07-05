6863cc180005146dbcd91e5455f3f83c68e839db
Geordie J <geojay@gmail.com>
2019-07-05 17:42:34 +0200

    Log

--- cmake/caches/android-armv7-swift-flags.cmake
+++ cmake/caches/android-armv7-swift-flags.cmake
@@ -8,6 +8,6 @@ set(CMAKE_SWIFT_LINK_FLAGS
 -tools-directory ${CMAKE_ANDROID_NDK}/toolchains/llvm/prebuilt/windows-x86_64/bin
 -Xclang-linker --gcc-toolchain=${CMAKE_ANDROID_NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/windows-x86_64
 -Xclang-linker --sysroot=${CMAKE_ANDROID_NDK}/platforms/android-${CMAKE_ANDROID_API}/arch-arm
--Xclang-linker -fuse-ld=gold.exe
+-Xclang-linker -fuse-ld=gold
 CACHE STRING "")
 
