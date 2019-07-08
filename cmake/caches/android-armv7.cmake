688d4537b6d3d8282101aadcaa1993d83a214cbc
Geordie J <geojay@gmail.com>
2019-07-08 17:53:53 +0200

    Add sccache to SDK builds

--- cmake/caches/android-armv7.cmake
+++ cmake/caches/android-armv7.cmake
@@ -5,3 +5,8 @@ set(ANDROID_ARM_NEON YES CACHE BOOL "")
 set(ANDROID_TOOLCHAIN clang CACHE STRING "")
 set(ANDROID_PLATFORM_LEVEL 21 CACHE STRING "")
 set(ANDROID_STL c++_static CACHE STRING "")
+
+if(EXISTS /usr/bin/sccache)
+  set(CMAKE_C_COMPILER_LAUNCHER /usr/bin/sccache CACHE STRING "")
+  set(CMAKE_CXX_COMPILER_LAUNCHER /usr/bin/sccache CACHE STRING "")
+endif()
--- cmake/caches/android-x86_64.cmake
+++ cmake/caches/android-x86_64.cmake
@@ -14,3 +14,8 @@ set(ANDROID_ABI "x86_64" CACHE STRING "")
 set(ANDROID_TOOLCHAIN clang CACHE STRING "")
 set(ANDROID_PLATFORM_LEVEL 21 CACHE STRING "")
 set(ANDROID_STL c++_static CACHE STRING "")
+
+if(EXISTS /usr/bin/sccache)
+  set(CMAKE_C_COMPILER_LAUNCHER /usr/bin/sccache CACHE STRING "")
+  set(CMAKE_CXX_COMPILER_LAUNCHER /usr/bin/sccache CACHE STRING "")
+endif()
