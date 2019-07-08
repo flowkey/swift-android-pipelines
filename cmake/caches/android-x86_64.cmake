cef330ac4e27224419f83a7382ecbff84d61fc19
Geordie J <geojay@gmail.com>
2019-07-08 19:11:49 +0200

    Set vars based on toolchain.directory

--- cmake/caches/android-x86_64.cmake
+++ cmake/caches/android-x86_64.cmake
@@ -1,10 +1,17 @@
 set(ANDROID_ABI "x86_64" CACHE STRING "")
 
 set(ANDROID_TOOLCHAIN clang CACHE STRING "")
-set(ANDROID_PLATFORM 21 CACHE STRING "")
+set(ANDROID_PLATFORM android-21 CACHE STRING "")
+set(ANDROID_PLATFORM_LEVEL 21 CACHE STRING "")
 set(ANDROID_STL c++_static CACHE STRING "")
 
 if(EXISTS /usr/bin/sccache)
   set(CMAKE_C_COMPILER_LAUNCHER /usr/bin/sccache CACHE STRING "")
   set(CMAKE_CXX_COMPILER_LAUNCHER /usr/bin/sccache CACHE STRING "")
+endif()
+
+if (EXISTS $ENV{toolchain.directory})
+  set(CMAKE_C_COMPILER "$ENV{toolchain.directory}/usr/bin/clang")
+  set(CMAKE_CXX_COMPILER "$ENV{toolchain.directory}/usr/bin/clang++")
+  set(CMAKE_SWIFT_COMPILER "$ENV{toolchain.directory}/usr/bin/swiftc")
 endif()
