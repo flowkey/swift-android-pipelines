c56fcaffe7400d56bed41be394336e7037ccc0aa
Geordie J <geojay@gmail.com>
2019-07-09 17:01:35 +0200

    Remove cache because it's having issues

--- cmake/caches/android-aarch64.cmake
+++ cmake/caches/android-aarch64.cmake
@@ -3,8 +3,3 @@ set(ANDROID_ABI arm64-v8a CACHE STRING "")
 set(ANDROID_PLATFORM_LEVEL 21 CACHE STRING "")
 set(ANDROID_TOOLCHAIN clang CACHE STRING "")
 set(ANDROID_STL c++_static CACHE STRING "")
-
-if(EXISTS /usr/bin/sccache)
-  set(CMAKE_C_COMPILER_LAUNCHER /usr/bin/sccache CACHE STRING "")
-  set(CMAKE_CXX_COMPILER_LAUNCHER /usr/bin/sccache CACHE STRING "")
-endif()
--- cmake/caches/android-armv7.cmake
+++ cmake/caches/android-armv7.cmake
@@ -6,8 +6,3 @@ set(ANDROID_TOOLCHAIN clang CACHE STRING "")
 set(ANDROID_PLATFORM android-21 CACHE STRING "")
 set(ANDROID_PLATFORM_LEVEL 21 CACHE STRING "")
 set(ANDROID_STL c++_static CACHE STRING "")
-
-if(EXISTS /usr/bin/sccache)
-  set(CMAKE_C_COMPILER_LAUNCHER /usr/bin/sccache CACHE STRING "")
-  set(CMAKE_CXX_COMPILER_LAUNCHER /usr/bin/sccache CACHE STRING "")
-endif()
